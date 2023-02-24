a1e2e27ff565cd609144310d0900b50028310591
aaronurayan <124217518+aaronurayan@users.noreply.github.com>
2023-02-24 13:52:07 +1100

    Framework Complete

---
+++ Azimuth/GameStates/IGameState.cs
@@ -0,0 +1,14 @@
+﻿namespace Azimuth.GameStates
+{
+	public interface IGameState
+	{
+		// ReSharper disable once CommentTypo
+		// Resharper disable once IncosistentNaming
+		public string ID { get; }
+
+		public void Load();
+		public void Unload();
+		public void Update(float _deltaTime);
+		public void Draw();
+	}
+}
