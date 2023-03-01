d093938edfaeba1291f98c7613277bb50f0ed025
aaronurayan <124217518+aaronurayan@users.noreply.github.com>
2023-03-01 13:41:45 +1100

    config

--- Azimuth/Application.cs
+++ Azimuth/Application.cs
@@ -9,16 +9,24 @@ namespace Azimuth
 	{
 		public static Application Instance { get; private set; }
 
-		public static void Run(int _width, int _height, string _title, Color _color, Game _game)
+		public static void Run<GAME>() where GAME : Game, new()
 		{
-			Instance = new Application(_width, _height, _title, _color, _game);
+			if(Instance != null)
+			{
+				Console.WriteLine("[ERROR] Attempted to run application more than once!");
+
+				return;
+			}
+			
+			Instance = new Application(new GAME());
 			Instance.Run();
 		}
 		public Window Window { get; }
 		private readonly Game game;
-		private Application(int _width, int _height, string _name, Color _color, Game _game)
+		private Application(Game _game)
 		{
-			Window = new Window(_width, _height, _name, _color);
+			Config.Create();
+			Window = new Window();
 			game = _game;
 
 		}
